e62c3b49becaab7b8f35b5f83d92d5cdf40da3bf
abdullkadir1899 <mdabdulkadir1899@gmail.com>
2025-08-27 14:04:08 +0600

    love add

---
+++ love.js
@@ -0,0 +1,37 @@
+// function love add
+function addLove(){
+    const copy = parseInt(document.getElementById('love-btn').innerText)
+    const add = copy + 1;
+    document.getElementById('love-btn').innerText = add
+}
+
+
+
+
+document.getElementById('love1').addEventListener('click', function(){
+    addLove()
+})
+document.getElementById('love2').addEventListener('click', function(){
+    addLove()
+})
+document.getElementById('love3').addEventListener('click', function(){
+    addLove()
+})
+document.getElementById('love4').addEventListener('click', function(){
+    addLove()
+})
+document.getElementById('love5').addEventListener('click', function(){
+    addLove()
+})
+document.getElementById('love6').addEventListener('click', function(){
+    addLove()
+})
+document.getElementById('love7').addEventListener('click', function(){
+    addLove()
+})
+document.getElementById('love8').addEventListener('click', function(){
+    addLove()
+})
+document.getElementById('love9').addEventListener('click', function(){
+    addLove()
+})
